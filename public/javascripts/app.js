abe34a55149f0e4037a17905a1bbdccb3ba06534
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2021-02-20 10:28:31 +0530

    Moved service worker register function to script.js

--- public/javascripts/app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/serviceworker.js')
-        .then(reg => console.log('service worker registered'))
-        .catch(err => console.log('service worker not registered', err));
-}
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceworker.js')
+        .then(reg => console.log('service worker registered'))
+        .catch(err => console.log('service worker not registered', err));
+}
+
 const loadImage = (e) => {
     document.getElementById('viewImage').src = URL.createObjectURL(e.target.files[0]);
 }
