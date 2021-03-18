3ebc5af7b000a768e92d3c63eb363fc4f79c6588
Vishnu-C-Prasad <mr.vishnucp001@outlook.com>
2021-03-18 10:15:18 +0530

    Added setActiveTab function to set active tab in admin sidebar

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -20,22 +20,14 @@ $('#updateProfilePicForm').submit((e) => {
     $('#profilePicLoadingBtn').removeAttr('hidden');
 });
 
-const editAdminDetails = (e) => {
-    e.preventDefault();
-    document.getElementById('name').removeAttribute('readonly');
-    document.getElementById('email').removeAttribute('readonly');
-    document.getElementById('save').removeAttribute('hidden');
-    document.getElementById('cancel').removeAttribute('hidden');
-    document.getElementById('edit').setAttribute('hidden', 'true');
-}
-
-const cancelEditAdminDetails = (e) => {
-    e.preventDefault();
-    document.getElementById('name').setAttribute('readonly', 'true');
-    document.getElementById('email').setAttribute('readonly', 'true');
-    document.getElementById('save').setAttribute('hidden', 'true');
-    document.getElementById('cancel').setAttribute('hidden', 'true');
-    document.getElementById('edit').removeAttribute('hidden');
+const setActiveTab = (id) => {
+    document.getElementById('dashboard-tab').classList.remove("active");
+    document.getElementById('theatre-management-tab').classList.remove("active");
+    document.getElementById('users-management-tab').classList.remove("active");
+    document.getElementById('users-activity-tab').classList.remove("active");
+    document.getElementById('admin-profile-tab').classList.remove("active");
+    
+    document.getElementById(`${id}`).classList.add("active");
 }
 
 const editTheatreDetails = (e) => {
